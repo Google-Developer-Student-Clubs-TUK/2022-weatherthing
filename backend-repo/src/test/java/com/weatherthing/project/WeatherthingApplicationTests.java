b6db2b6dea376e7052fc5bbaf8abbcda091f1939
soohyun <vivian0304@naver.com>
2022-12-01 17:44:09 +0900

    feat: 초기설정 및 채팅 기능 구현

---
+++ backend-repo/src/test/java/com/weatherthing/project/WeatherthingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherthing.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
