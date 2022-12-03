d3dab04e7a431ed2ca8b2009d7b2dc97e6386173
Jung Yunho <a01049048063@gmail.com>
2022-12-03 20:31:41 +0900

    feat: user entity 변경

--- backend-repo/src/main/java/com/weatherthing/project/newboard/entity/User.java
+++ backend-repo/src/main/java/com/weatherthing/project/newboard/entity/User.java
@@ -10,20 +10,26 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
-    private int uId;
-    //@Email 어노테이션 적용이 안됨
+    @Column(nullable = false)
+    private Long uId;
+
+    @Column(nullable = false)//@Email 어노테이션 적용이 안됨
     private String email;
 
+    @Column(nullable = false)
     private String nickname;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING) //Enum타입을 상수로 db에 저장
     private Gender gender;
 
-    private int age;
+    @Column(nullable = false)
+    private Long age;
 
-    private int weahter;
+    @Column(nullable = false)
+    private Long weahter;
 
     @CreationTimestamp //생성시간 저장
     private Timestamp createDate;
