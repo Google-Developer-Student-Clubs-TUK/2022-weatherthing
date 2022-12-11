f3da00163aa0a33e162c392e5eb09165f0c27ebc
Jung Yunho <a01049048063@gmail.com>
2022-12-11 16:19:13 +0900

    feat: basetimeentity 생성

---
+++ backend-repo/src/main/java/com/weatherthing/project/board/entity/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.weatherthing.project.board.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate  //최초 생성 시점
+    private LocalDateTime createAt;
+
+    @LastModifiedDate  // 마지막 변경 시점
+    private LocalDateTime updateAt;
+}
